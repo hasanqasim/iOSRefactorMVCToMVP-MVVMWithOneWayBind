98a96692fdf35e5e958023c5fd90c2a2654deb47
Hasan Qasim <hasanqasim92@gmail.com>
2020-09-07 13:08:27 +1000

    refactor data model and add mutating functions, a computed property and a custom subscript

---
+++ RefactorMVCToMVP/Model/Model.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  RefactorMVCToMVP
+//
+//  Created by Hasan Qasim on 7/9/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import Foundation
+
+struct Model<T> {
+    private var objects = [T]()
+    
+    mutating func insert(_ element: T) {
+        objects.insert(element, at: 0)
+    }
+    
+    mutating func remove(at index: Int) {
+        guard objects.indices.contains(index) else {
+            return
+        }
+        objects.remove(at: index)
+    }
+    
+    // use an explicit type with computed properties
+    var counts: Int {
+        return objects.count
+    }
+    
+    // implement a custom subscript to access models element by index
+    subscript(index: Int) -> T? {
+        return objects.indices.contains(index) ? objects[index] : nil
+    }
+}
